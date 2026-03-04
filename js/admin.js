50525508321f438a39f306f6d72ec717f0a24bf2
Gabor Angyal <gabor.angyal@codesharp.dev>
2026-03-04 15:45:51 +0100

    bugfix

--- js/admin.js
+++ js/admin.js
@@ -1,25 +1,72 @@
 jQuery(document).ready(function($) {
-    $('#vt-date-picker').datepicker({
-        dateFormat: 'yy-mm-dd',
-        maxDate: 0
-    });
-    
     $('#vt-filter-btn').on('click', function() {
-        var selectedDate = $('#vt-date-picker').val();
+        var selectedCheckpointId = $('#vt-checkpoint-selector').val();
         
         $.ajax({
             url: versionTrackerAdmin.ajaxurl,
             type: 'POST',
             data: {
-                action: 'version_tracker_get_versions',
-                date: selectedDate
+                action: versionTrackerAdmin.getVersionsAction,
+                checkpoint_id: selectedCheckpointId
             },
             success: function(response) {
                 var data = JSON.parse(response);
-                $('#vt-versions-container').html(data.html);
+                if (data.error) {
+                    alert('Error: ' + data.error);
+                } else {
+                    $('#vt-versions-container').html(data.html);
+                }
             },
             error: function() {
                 alert('Error loading versions');
+            }
+        });
+    });
+    
+    $('#vt-create-checkpoint-btn').on('click', function() {
+        $.ajax({
+            url: versionTrackerAdmin.ajaxurl,
+            type: 'POST',
+            data: {
+                action: versionTrackerAdmin.createCheckpointAction
+            },
+            success: function(response) {
+                var data = JSON.parse(response);
+                if (data.success) {
+                    alert('Checkpoint created successfully');
+                    location.reload();
+                } else {
+                    alert('Error: ' + data.error);
+                }
+            },
+            error: function() {
+                alert('Error creating checkpoint');
+            }
+        });
+    });
+    
+    $('#vt-delete-checkpoint-btn').on('click', function() {
+        if (!confirm('Are you sure you want to delete the last checkpoint?')) {
+            return;
+        }
+        
+        $.ajax({
+            url: versionTrackerAdmin.ajaxurl,
+            type: 'POST',
+            data: {
+                action: versionTrackerAdmin.deleteCheckpointAction
+            },
+            success: function(response) {
+                var data = JSON.parse(response);
+                if (data.success) {
+                    alert('Checkpoint deleted successfully');
+                    location.reload();
+                } else {
+                    alert('Error: ' + data.error);
+                }
+            },
+            error: function() {
+                alert('Error deleting checkpoint');
             }
         });
     });
